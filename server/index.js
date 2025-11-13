4c81e5adea8aa1c45e651c03cbb714865552e1c6
Kishan <codewithkishan119@gmail.com>
2025-11-13 22:22:48 +0530

    Port Error Solved

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
